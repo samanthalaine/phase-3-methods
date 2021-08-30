21601b08eede07a4d8e3f256b7a100b57bea3d31
Samantha Laine <samanthalaine8@gmail.com>
2021-08-30 00:48:39 -0400

    commit tree 53a7c5cef3eef7d22907a4602e2e878199fb7b47

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
 # Your code here!
+def greet_programmer
+    puts 'Hello, programmer!'
+end
+
+
+def greet (name)
+    puts "Hello, #{name}!"
+end
+greet 'Samie'
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default 'Samie'
+
+
+def add (num1, num2)
+    return num1 + num2
+end
+
+def halve (number)
+    return nil unless number.class == Integer
+
+    number/2
+end
